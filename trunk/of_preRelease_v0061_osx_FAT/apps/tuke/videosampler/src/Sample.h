cc84a46fdf48323fe61526781494d8ce928710d4
bereza <bereza@3a5b5021-5be8-e3f2-3ea9-e40b785f49fe>
2010-04-27 16:50:14 +0000

    initial add of videosampler (doesn't do anything yet)

---
+++ trunk/of_preRelease_v0061_osx_FAT/apps/tuke/videosampler/src/Sample.h
@@ -0,0 +1,52 @@
+/*
+ *  Sample.h
+ *  openFrameworks
+ *
+ *  Created by Marek Bereza on 17/01/2009.
+ *  Copyright 2009 Royal College of Art. All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <string>
+
+
+#ifndef SAMPLE
+#define SAMPLE
+
+//#define SHALLOWCOPY 
+
+using namespace std;
+class Sample {
+	
+public:
+	
+	Sample();
+	void load(float* data, int length);
+	~Sample() {
+		//delete [] data;
+	};
+	void dealloc() {
+#ifndef SHALLOWCOPY 
+		delete [] data;
+#endif
+	}
+	void trigger(float volume);
+	void getSamples(float* out, int length);
+	void addSamples(float* out, int length);
+	float getSample();
+	float getSample(float speed);
+	void stop();
+	void normalize();
+	bool playing;
+	
+	int length;
+	float *data;
+	float volume;
+
+private:
+	float pos;
+	
+	
+};
+#endif
+
